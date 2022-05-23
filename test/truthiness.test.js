dbafddb16cbd6e344e2820d439607cd2e6df06a9
dyaksaa <diasnour0395@gmail.com>
2022-05-23 10:58:40 +0700

    number matchers

---
+++ test/truthiness.test.js
@@ -0,0 +1,13 @@
+test("test toBeNull", () => {
+  let value = null;
+  expect(value).toBeNull();
+  expect(value).toBeDefined();
+  expect(value).toBeFalsy();
+
+  value = undefined;
+  expect(value).toBeUndefined();
+  expect(value).toBeFalsy();
+
+  value = "Dyaksa";
+  expect(value).toBeTruthy();
+});
